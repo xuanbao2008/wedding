f48578e7e1693a2848d21599a521eb22a579219f
Do Thi Thao <xuanbao2008@gmail.com>
2026-05-17 02:33:01 +0900

    fix: revert build script to next build to avoid recursive invocation

--- scripts/build.js
+++
@@ -1,9 +0,0 @@
-const { execSync } = require('child_process')
-
-if (process.env.CF_PAGES) {
-  console.log('Detected Cloudflare Pages, running @cloudflare/next-on-pages...')
-  execSync('npx @cloudflare/next-on-pages@1', { stdio: 'inherit' })
-} else {
-  console.log('Running standard Next.js build...')
-  execSync('next build', { stdio: 'inherit' })
-}
